c721c8bb76e32772b7aa17ebb621d2d324fdb57e
Mohamed Amr <81720517+mohamedamr987@users.noreply.github.com>
2023-04-21 00:05:45 +0200

    changed logic of searching

--- lib/allReportsPage/repo/all_reports_repo.dart
+++ lib/allReportsPage/repo/all_reports_repo.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
-import 'package:mafqood/core/core_info.dart';
 import 'package:mafqood/core/errors/failures.dart';
 import 'package:mafqood/core/models/category_model.dart';
 import 'package:mafqood/core/models/report_model.dart';
@@ -11,26 +10,36 @@ class AllReportsRepo extends Repository {
 
   Future<Either<Failure, List<ReportModel>>> getReportsByCategory(CategoryModel categoryModel) async {
     return await exceptionHandler<List<ReportModel>>(
-          () async {
+      () async {
         List<ReportModel> reports = [];
-        await firestore.collection('reports').where("category_id", isEqualTo: categoryModel.id).get().then((value) => value.docs.forEach((element) {
-          reports.add(ReportModel.fromJson(element.data(),element.id));
-        }));
-        return reports.reversed.toList();
+        reports = (await getReports()).where((element) => element.categoryId == categoryModel.id).toList();
+        return reports;
       },
     );
   }
 
   Future<Either<Failure, List<ReportModel>>> searchReports(String name) async {
+    name = name.toLowerCase();
     return await exceptionHandler<List<ReportModel>>(
           () async {
         List<ReportModel> reports = [];
-        await firestore.collection('reports').where("name", arrayContains: name).where("place", arrayContains: name).get().then((value) => value.docs.forEach((element) {
-          reports.add(ReportModel.fromJson(element.data(),element.id));
-        }));
-        return reports.reversed.toList();
+        reports = (await getReports())
+            .where((element) =>
+                element.name.toLowerCase().contains(name) ||
+                element.place.toLowerCase().contains(name) ||
+                element.age.toLowerCase().contains(name))
+            .toList();
+        return reports;
       },
     );
+  }
+
+  Future<List<ReportModel>> getReports() async {
+    List<ReportModel> reports = [];
+    await firestore.collection('reports').get().then((value) => value.docs.forEach((element) {
+      reports.add(ReportModel.fromJson(element.data(),element.id));
+    }));
+    return reports.reversed.toList();
   }
 
 }
--- lib/homePage/components/home_search_component.dart
+++ lib/homePage/components/home_search_component.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mafqood/allReportsPage/all_reports_page_view.dart';
 import 'package:mafqood/constants.dart';
+import 'package:mafqood/core/helpers/navigation_helper.dart';
 import 'package:mafqood/homePage/controller.dart';
-
-class HomeSearchComponent extends StatelessWidget {
+import 'package:mafqood/widgets/general_text.dart';
+class HomeSearchComponent extends StatefulWidget {
   const HomeSearchComponent({Key? key}) : super(key: key);
+
+  @override
+  State<HomeSearchComponent> createState() => _HomeSearchComponentState();
+}
+
+class _HomeSearchComponentState extends State<HomeSearchComponent> {
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +20,23 @@ class HomeSearchComponent extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 30),
       child: TextField(
         controller: blocController.searchController,
+        onChanged: (value) {
+          setState(() {});
+        },
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.symmetric(vertical: 0),
           hintText: "Search",
           hintStyle: TextStyle(color: kPrimaryColor.withOpacity(0.7),),
           prefixIcon: Icon(Icons.search, color: kPrimaryColor.withOpacity(0.7),),
+          suffix: blocController.searchController.text.isNotEmpty ? GestureDetector(
+            onTap: () {
+              NavigationHelper.push(context, AllReportsPageView(name: blocController.searchController.text,),);
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: const GeneralText(text: "Search", color: kPrimaryColor,),
+            ),
+          ) : const SizedBox(),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(60),
             borderSide: const BorderSide(color: kPrimaryColor,),
